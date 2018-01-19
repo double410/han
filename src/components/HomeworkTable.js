4b72b7e7a55486399092234bad41762adda01510
Juan Du <juandu@JuandeAir.lan>
2018-01-19 22:41:01 +0800

    format the content

--- src/components/HomeworkTable.js
+++ src/components/HomeworkTable.js
@@ -1,7 +1,7 @@
 import 'react-dates/initialize';
 import React, { Component } from 'react';
-import { Form, Button, Card, Header, Grid} from "semantic-ui-react";
-import { SingleDatePicker } from "react-dates";
+import { Form, Button, Card, Header, Grid, List } from "semantic-ui-react";
+import { SingleDatePicker, DayPickerRangeController } from "react-dates";
 import homework from "../data/sample.json";
 import 'react-dates/lib/css/_datepicker.css';
 
@@ -16,28 +16,38 @@ class HomeworkTable extends Component {
 
     render() {
         const items = homework.classes.map((item, index) => {
-            return(
+            let contents = item.content.split('\n');
+            if (Array.isArray(contents)) {
+                contents = contents.map((content, index) => {
+                    return (
+                        <List.Item key={index}>
+                            {content}
+                        </List.Item>
+                    );
+                });
+            }
+
+            return (
                 <Card.Content key={index}>
                     <Card.Header>{item.subject}</Card.Header>
-                    <Card.Description>                        
-                        <div>{item.content}</div>
+                    <Card.Description>
+                        {contents}
                     </Card.Description>
                 </Card.Content>
             );
         });
-// https://github.com/airbnb/react-dates
+        // https://github.com/airbnb/react-dates
         console.log(this.state.date);
 
         return (
             <Grid column={1}>
                 <Grid.Column width={10}>
                     <Form>
-                        <Form.Input label='请选择日期：' placeholder='dd/mm/yyyy' />
-                        <SingleDatePicker date={this.state.date} onDateChange={date => this.setState({date})} onFocusChange={focusedInput => this.setState({ focusedInput })}/>
-                        {/* <From.Field label='请选择日期：' control={CustomCalendar} start='1' end='7' /> */}
-                        <Button color='teal'  type='submit'>查询</Button>
+                        {/* <Form.Input label='请选择日期：' placeholder='dd/mm/yyyy' /> */}
+                        <SingleDatePicker date={this.state.date} onDateChange={date => this.setState({ date })} onFocusChange={focusedInput => this.setState({ focusedInput })} />
+                        <Button color='teal' type='submit'>查询</Button>
                     </Form>
-                    <Header color='teal'>{homework.title}</Header>            
+                    <Header color='teal'>{homework.title}</Header>
                     <Card fluid color='teal'>
                         {items}
                     </Card>
