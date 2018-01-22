51e9abf0193d63de9e749af47ed0ffff0662be84
Fred Chen <baoqiang.chen@dxc.com>
2018-01-22 16:21:26 +0800

    adjust the date-picker

--- src/components/HomeworkTable.js
+++ src/components/HomeworkTable.js
@@ -1,16 +1,18 @@
 import 'react-dates/initialize';
 import React, { Component } from 'react';
 import { Form, Button, Card, Header, Grid, List } from "semantic-ui-react";
-import { SingleDatePicker, DayPickerRangeController } from "react-dates";
+import { SingleDatePicker } from "react-dates";
 import homework from "../data/sample.json";
-import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
+
 
 class HomeworkTable extends Component {
     constructor(props) {
         super(props);
-        // const now = Date.now();
+        moment.locale('zh-cn');
+        console.log(moment([2015, 1, 2]).fromNow());
         this.state = {
-            date: props.initialDate,
+            date: moment(),
         };
     }
 
@@ -41,11 +43,22 @@ class HomeworkTable extends Component {
 
         return (
             <Grid column={1}>
-                <Grid.Column width={10}>
+                <Grid.Column>
                     <Form>
                         {/* <Form.Input label='请选择日期：' placeholder='dd/mm/yyyy' /> */}
-                        <SingleDatePicker date={this.state.date} onDateChange={date => this.setState({ date })} onFocusChange={focusedInput => this.setState({ focusedInput })} />
-                        <Button color='teal' type='submit'>查询</Button>
+                        <Form.Group>
+                            <SingleDatePicker                                
+                                date={this.state.date} // momentPropTypes.momentObj or null
+                                onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
+                                focused={this.state.focused} // PropTypes.bool
+                                onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+                                numberOfMonths={1}
+                                isOutsideRange={()=>null}
+                                monthFormat = "YYYY[年]MMMM"
+                                displayFormat={() => moment.localeData().longDateFormat('L')}
+                            />
+                            <Button color='teal' type='submit'>查询</Button>
+                        </Form.Group>
                     </Form>
                     <Header color='teal'>{homework.title}</Header>
                     <Card fluid color='teal'>
